c02378e639739801f68a029d9c9a9c5c92f18e5d
Brandon Williams <brandon@opetopic.com>
2010-08-09 16:52:32 -0400

    Initial commit.

---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  IAAS
+//
+//  Created by Brandon Williams on 8/9/10.
+//  Copyright Opetopic 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
